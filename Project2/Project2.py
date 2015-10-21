9f2a25a1fea6ce2ec6ad0c0f2651b616b14c9a9b
Michael Gu <mgu@csumb.edu>
2015-10-21 10:28:13 -0700

    Updated Try Again Option
    Final version before submission

--- Project2/Project2.py
+++ Project2/Project2.py
@@ -2,60 +2,67 @@ import image_slicer
 from PIL import Image
 from PIL import ImageDraw, ImageFont
 
+play = 0
+score = 4
 
-tiles = image_slicer.slice(input('Enter File Name:'), 4, save=False)
+while play == 0: 
 
-for tile in tiles:
-    overlay = ImageDraw.Draw(tile.image)
+    tiles = image_slicer.slice(input('Enter File Name:'), 4, save=False)
 
-image_slicer.save_tiles(tiles)
+    for tile in tiles:
+        overlay = ImageDraw.Draw(tile.image)
 
-image1 = Image.open('C:\\Users\\Michael\\Documents\\205Project2\\Project2\\_02_02.png')
-image2 = Image.open('C:\\Users\\Michael\\Documents\\205Project2\\Project2\\_02_01.png')
-image3 = Image.open('C:\\Users\\Michael\\Documents\\205Project2\\Project2\\_01_02.png')
-image4 = Image.open('C:\\Users\\Michael\\Documents\\205Project2\\Project2\\_01_01.png')
+    image_slicer.save_tiles(tiles)
 
-width,height = image1.size
-width = (width * 2) + 20
-height = (height * 2) + 20
-jigsaw = Image.new('RGB',(width,height))
-width,height = image1.size
-jigsaw.paste(image1,(0,0))
-jigsaw.paste(image2,(width + 10,0))
-jigsaw.paste(image3,(0,height + 10))
-jigsaw.paste(image4,(width + 10, height + 10))
+    image1 = Image.open('C:\\Users\\Michael\\Documents\\205Project2\\Project2\\_02_02.png')
+    image2 = Image.open('C:\\Users\\Michael\\Documents\\205Project2\\Project2\\_02_01.png')
+    image3 = Image.open('C:\\Users\\Michael\\Documents\\205Project2\\Project2\\_01_02.png')
+    image4 = Image.open('C:\\Users\\Michael\\Documents\\205Project2\\Project2\\_01_01.png')
 
-jigsaw.show()
+    width,height = image1.size
+    width = (width * 2) + 20
+    height = (height * 2) + 20  
+    jigsaw = Image.new('RGB',(width,height))
+    width,height = image1.size
+    jigsaw.paste(image1,(0,0))
+    jigsaw.paste(image2,(width + 10,0))
+    jigsaw.paste(image3,(0,height + 10))
+    jigsaw.paste(image4,(width + 10, height + 10))
 
-picture1 = 0
-while picture1 != 4:
-    picture1 = int(input("Where should the top left square be?\n1. Top left\n2. Bottom Left\n3. Top right\n4. Bottom right\n"))
-    if picture1 != 4:
-        print("Incorrect Try again")
-print("Correct!")
+    jigsaw.show()
 
-while picture1 != 2:
-    picture1 = int(input("Where should the top right square be?\n1. Top left\n2. Bottom Left\n3. Top right\n4. Bottom right\n"))
-    if  picture1 != 2:
-        print("Incorrect Try again")
-print("Correct!")
+    picture1 = 0
+    while picture1 != 4:
+        picture1 = int(input("Where should the top left square be?\n1. Top left\n2. Bottom Left\n3. Top right\n4. Bottom right\n"))
+        if picture1 != 4:
+            print("Incorrect Try again")
+    print("Correct!")
 
-while picture1 != 1:
-    picture1 = int(input("Where should the bottom left square be?\n1. Top left\n2. Bottom Left\n3. Top right\n4. Bottom right\n"))
-    if  picture1 != 1:
-        print("Incorrect Try again")
-print("Correct!")
+    while picture1 != 2:
+        picture1 = int(input("Where should the top right square be?\n1. Top left\n2. Bottom Left\n3. Top right\n4. Bottom right\n"))
+        if  picture1 != 2:
+            print("Incorrect Try again")
+    print("Correct!")
 
-while picture1 != 3:
-    picture1 = int(input("Where should the bottom right square be?\n1. Top left\n2. Bottom Left\n3. Top right\n4. Bottom right\n"))
-    if  picture1 != 3:
-        print("Incorrect Try again")
-print("Correct!")
-print ("Jigsaw Solved!")
+    while picture1 != 1:
+        picture1 = int(input("Where should the bottom left square be?\n1. Top left\n2. Bottom Left\n3. Top right\n4. Bottom right\n"))
+        if  picture1 != 1:
+            print("Incorrect Try again")
+    print("Correct!")
 
-jigsaw.paste(image4,(0,0))
-jigsaw.paste(image3,(width + 10,0))
-jigsaw.paste(image2,(0,height + 10))
-jigsaw.paste(image1,(width + 10, height+ 10))
+    while picture1 != 3:
+        picture1 = int(input("Where should the bottom right square be?\n1. Top left\n2. Bottom Left\n3. Top right\n4. Bottom right\n"))
+        if  picture1 != 3:
+            print("Incorrect Try again")
+    print("Correct!")
+    print ("Jigsaw Solved!")
 
-jigsaw.show()
+    jigsaw.paste(image4,(0,0))
+    jigsaw.paste(image3,(width + 10,0))
+    jigsaw.paste(image2,(0,height + 10))
+    jigsaw.paste(image1,(width + 10, height+ 10))
+
+    jigsaw.show()
+
+    play = int(input("Play again? Type 0 to play again or any other number to exit: "))
+               
